3354436e4c2c6b7fe7db41d32353fd4d5fad1374
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-07-09 20:48:27 -0300

    cool

--- js/api/BiggestNumber.js
+++ js/api/BiggestNumber.js
@@ -37,37 +37,103 @@ function BiggestNumber() {
     this.fourOfPower = (pow) => new Decimal.pow(4, pow)
 
     /**
-     * Converts Two Power
-     * @param {number} pow Convert Has "2 ** x" (or "Math.pow(2, x)")
-     * @returns {string} Convert to 2 Of pow
+     * Converts Five Power
+     * @param {number} pow Convert Has "5 ** x" (or "Math.pow(5, x)")
+     * @returns {string} Convert to 5 Of pow
      */
-     this.fiveOfPower = (pow) => new Decimal.pow(2, pow)
+     this.fiveOfPower = (pow) => new Decimal.pow(5, pow)
 
      /**
-     * Converts Two Power
-     * @param {number} pow Convert Has "2 ** x" (or "Math.pow(2, x)")
-     * @returns {string} Convert to 2 Of pow
+     * Converts Six Power
+     * @param {number} pow Convert Has "6 ** x" (or "Math.pow(6, x)")
+     * @returns {string} Convert to 6 Of pow
      */
-    this.sixOfPower = (pow) => new Decimal.pow(2, pow)
+    this.sixOfPower = (pow) => new Decimal.pow(6, pow)
 
     /**
-     * Converts Two Power
-     * @param {number} pow Convert Has "2 ** x" (or "Math.pow(2, x)")
-     * @returns {string} Convert to 2 Of pow
+     * Converts Seven Power
+     * @param {number} pow Convert Has "7 ** x" (or "Math.pow(7, x)")
+     * @returns {string} Convert to 7 Of pow
      */
-     this.sevenOfPower = (pow) => new Decimal.pow(2, pow)
+     this.sevenOfPower = (pow) => new Decimal.pow(7, pow)
 
      /**
-     * Converts Two Power
-     * @param {number} pow Convert Has "2 ** x" (or "Math.pow(2, x)")
-     * @returns {string} Convert to 2 Of pow
+     * Converts Eight Power
+     * @param {number} pow Convert Has "8 ** x" (or "Math.pow(8, x)")
+     * @returns {string} Convert to 8 Of pow
      */
-    this.eightOfPower = (pow) => new Decimal.pow(2, pow)
+    this.eightOfPower = (pow) => new Decimal.pow(8, pow)
 
     /**
-     * Converts Two Power
-     * @param {number} pow Convert Has "2 ** x" (or "Math.pow(2, x)")
-     * @returns {string} Convert to 2 Of pow
+     * Converts Nine Power
+     * @param {number} pow Convert Has "9 ** x" (or "Math.pow(9, x)")
+     * @returns {string} Convert to 9 Of pow
      */
-     this.nineOfPower = (pow) => new Decimal.pow(2, pow)
+     this.nineOfPower = (pow) => new Decimal.pow(9, pow)
+
+     /**
+     * Converts Ten Power
+     * @param {number} pow Convert Has "10 ** x" (or "Math.pow(10, x)")
+     * @returns {string} Convert to 10 Of pow
+     */
+      this.tenOfPower = (pow) => new Decimal.pow(10, pow)
+
+      /**
+     * Converts Hundred Power
+     * @param {number} pow Convert Has "100 ** x" (or "Math.pow(100, x)")
+     * @returns {string} Convert to 100 Of pow
+     */
+     this.hundredOfPower = (pow) => new Decimal.pow(100, pow)
+
+     /**
+     * Converts Thousand Power
+     * @param {number} pow Convert Has "1000 ** x" (or "Math.pow(1000, x)")
+     * @returns {string} Convert to 1000 Of pow
+     */
+      this.thousandOfPower = (pow) => new Decimal.pow(1e3, pow)
+
+      /**
+     * Converts Ten Thousand Power
+     * @param {number} pow Convert Has "1e4 ** x" (or "Math.pow(1e4, x)")
+     * @returns {string} Convert to 1e4 Of pow
+     */
+     this.tenThousandOfPower = (pow) => new Decimal.pow(1e4, pow)
+
+     /**
+     * Converts Hundred Thousand Power
+     * @param {number} pow Convert Has "1e5 ** x" (or "Math.pow(1e5, x)")
+     * @returns {string} Convert to 1e5 Of pow
+     */
+      this.hundredThousandOfPower = (pow) => new Decimal.pow(1e5, pow)
+
+      /**
+     * Converts Million Power
+     * @param {number} pow Convert Has "1e6 ** x" (or "Math.pow(1e6, x)")
+     * @returns {string} Convert to 1e6 Of pow
+     */
+       this.hundredThousandOfPower = (pow) => new Decimal.pow(1e6, pow)
+
+    /**
+     * Addiontial Math
+     * @param {number} a Addiontal Math is done know a + b from mulitipler.
+     * @param {number} b Addiontal Letter 2
+     * @returns {string} Convert to Addiontail
+     */
+     this.addiontial = (a, b) => new Decimal(a).add(b) 
+
+    /**
+     * Mulitipler Math
+     * @param {number} a Mulitipler Letter 1 know a * b from power.
+     * @param {number} b Mulitipler Letter 2
+     * @returns {string} Convert to Addiontail
+     */
+     this.mulitipler = (a, b) => new Decimal(a).mul(b) 
+
+     /**
+     * Divide Math
+     * @param {number} a Divide Letter 1
+     * @param {number} b Divide Letter 2
+     * @returns {string} Convert to Divide
+     */
+      this.addiontial = (a, b) => new Decimal(a).div(b) 
 }
